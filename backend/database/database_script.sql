c5a090824d4a86422453e549e8d40df739c2ee93
Miguel Cabrera <miguelangelcabreravictoria@gmail.com>
2023-03-29 20:19:33 -0600

    ejs (view engine) installed

--- backend/database/database_script.sql
+++ backend/database/database_script.sql
@@ -1,4 +1,20 @@
-CREATE DATABASE IF NOT EXISTS damijoda;
+--Create database
+CREATE DATABASE IF NOT EXISTS basededatos;
 
-USE damijoda;
+USE basededatos;
+
+--users table 
+
+CREATE TABLE users (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    passoword VARCHAR(50) NOT NULL,
+    login_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    PRIMARY KEY(id)
+);
+
+DESCRIBE users;
+
+--
+
 
