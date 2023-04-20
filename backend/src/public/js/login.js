225216cdc1d32d4a6963beaafb72e22ad7430654
daniel-rdcz <danield_895@hotmail.com>
2023-04-20 12:51:19 -0600

    Estilizado de login page

---
+++ backend/src/public/js/login.js
@@ -0,0 +1,20 @@
+document.getElementById('login-button').addEventListener('click', openMenuBox);
+
+let h2 = document.getElementById('h2');
+let form_box = document.getElementById('form-box');
+let login_logo_button = document.getElementById('login-logo-button');
+let function_move =  document.getElementById('functions');
+let section = document.getElementById('section-background');
+
+function openMenuBox() {
+    h2.classList.toggle('h2-move');
+    form_box.classList.toggle('form-box-move');
+    section.classList.toggle('section-background-change');
+}
+
+let loginButton = document.getElementById('login-button');
+
+loginButton.addEventListener('click', () => {
+  document.body.classList.toggle('color-changed');
+});
+  
